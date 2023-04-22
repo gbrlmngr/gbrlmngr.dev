52d246e0f9a4bbe1455105d4ec425af82a27b2be
Gabriel Mangiurea <gabriel.mangiurea+git@outlook.com>
2023-04-22 13:23:13 +0300

    build(deps): add Vercel Analytics

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,11 +1,13 @@
 import "@/styles/globals.css";
 import type { AppProps } from "next/app";
+import { Analytics } from "@vercel/analytics/react";
 import { NextIntlProvider } from "next-intl";
 
 const App = ({ Component, pageProps }: AppProps) => {
   return (
     <NextIntlProvider messages={pageProps.i18n}>
       <Component {...pageProps} />
+      <Analytics />
     </NextIntlProvider>
   );
 };
